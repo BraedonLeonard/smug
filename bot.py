e01b62980dfaf5f39f795d3f15b6a3f44b20eb87
William Da Silva <WillDaSilva@users.noreply.github.com>
2018-01-14 07:43:21 +0000

    Add commands.Bot.longSay function

--- bot.py
+++ bot.py
@@ -3,10 +3,11 @@
 
 import logging
 import textwrap
+
+from discord.ext import commands
 import yaml
 
-import discord
-from discord.ext import commands
+import util
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -30,6 +31,28 @@ async def on_ready():
         ------------------
         '''
     ))
+
+async def longSay(self, fullMessage, useCodeBlocks=False):
+    '''
+    Allows the bot to send messages longer than the 2000 character limit imposed
+    by Discord by sending the message as a series of (up to) 2000 character
+    messages.
+    Sends up to messageLimit messages.
+    '''
+    messageLimit = 10 # 2000 characters is too little, but 20000 is a lot
+    # Creating a code block takes 7 characters
+    characterLimit = 2000 - 7 if useCodeBlocks else 2000
+    breakIndexes = range(0, len(fullMessage), characterLimit)
+    messages = [fullMessage[i:i+characterLimit] for i in breakIndexes]
+    if useCodeBlocks:
+        messages = [util.markdownCodeBlock(message) for message in messages]
+    for message in messages[:messageLimit]:
+        await self.say(message)
+    if len(messages) > messageLimit:
+        await self.say(''.join((
+            f'Exceeded message limit ({messageLimit}). ',
+            f'{len(messages) - messageLimit} messages not sent.')))
+commands.Bot.longSay = longSay
 
 # load, unload, and reload are provided to aid in the development of the bot
 # A developer can write their changes, then reload the module they changed

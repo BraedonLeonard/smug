16fd6bd32ba53b5c1fcc74f68b6a47737ce74894
William Da Silva <WillDaSilva@users.noreply.github.com>
2018-01-14 02:23:09 +0000

    Add markdownCodeBlock formatter function

--- util.py
+++ util.py
@@ -1,13 +1,17 @@
 import re
 
-'''Various helper functions used by the bot'''
+''' Various helper functions used by the bot '''
 
 def static_vars(**kwargs):
-    '''Decorator for assigning static variables to a function'''
+    ''' Decorator for assigning static variables to a function '''
     def decorate(func):
         for k in kwargs:
             setattr(func, k, kwargs[k])
         return func
     return decorate
+
+def markdownCodeBlock(text: str, language: str=''):
+    ''' formats text as a markdown code block '''
+    return '\n'.join([f'```{language}'] + text.split('\n') + ['```'])
 
 NdN = re.compile(r'\d+d\d+')
